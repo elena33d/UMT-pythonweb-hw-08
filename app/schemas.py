9714838a5329c13d189cfeb119a18c80de939b44
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-05-26 16:17:37 +0200

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,31 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
 
+class ContactBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone: str
+    birthday: date
+    additional_data: Optional[str] = None
+
+
+class ContactCreate(ContactBase):
+    pass
+
+
+class ContactUpdate(BaseModel):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    birthday: Optional[date] = None
+    additional_data: Optional[str] = None
+
+
+class ContactResponse(ContactBase):
+    id: int
+
+    class Config:
+        orm_mode = True
