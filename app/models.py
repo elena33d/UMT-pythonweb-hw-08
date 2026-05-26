094d5a3373a9af1e0a40f0a5b10e125b4b24af6c
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-05-26 16:17:29 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String, Date
+from database import Base
 
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    phone = Column(String)
+    birthday = Column(Date)
+    additional_data = Column(String, nullable=True)
